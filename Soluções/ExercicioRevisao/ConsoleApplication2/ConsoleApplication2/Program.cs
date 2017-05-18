5648d962829fb7b5c33948ce7b1f863e3694d852
Arthur <cogupl4ys@gmail.com>
2017-05-18 14:07:34 -0300

    Etapa 2(e 1) Aqui

--- Soluções/ExercicioRevisao/ConsoleApplication2/ConsoleApplication2/Program.cs
+++ Soluções/ExercicioRevisao/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -11,30 +11,43 @@ namespace ConsoleApplication1
         {
             string nome, T, P;
             double km, pot;
+            int i, qtd;
 
-            Console.WriteLine("Informe o modelo do carro");
-            nome = Console.ReadLine();
+            Console.WriteLine("Informe a quantidade de carros");
+            qtd = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Informe a quilometragem do carro");
-            km = Convert.ToDouble(Console.ReadLine());
+            for (i = 0; i < qtd; i++)
+            {
+                Console.WriteLine("Informe o modelo do carro");
+                nome = Console.ReadLine();
 
-            Console.WriteLine("Informe a potencia do carro");
-            pot = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Informe a quilometragem do carro");
+                km = Convert.ToDouble(Console.ReadLine());
 
-            if (km < 5000)
-                T = "novo";
-            else if (km >= 5000 && km <= 30000)
-                T = "seminovo";
-            else
-                T = "velho";
-            if (pot > 200)
-                P = "potente";
-            else if (pot > 120 && pot <= 200)
-                P = "forte";
-            else
-                P = "popular";
+                Console.WriteLine("Informe a potencia do carro");
+                pot = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("O {0} - {1} - {2}", nome, T, P);
+                if (km < 5000)
+                    T = "novo";
+
+                else if (km >= 5000 && km <= 30000)
+                    T = "seminovo";
+
+                else
+                    T = "velho";
+
+                if (pot > 200)
+                    P = "potente";
+
+                else if (pot > 120 && pot <= 200)
+                    P = "forte";
+
+                else
+                    P = "popular";
+                Console.WriteLine("O {0} - {1} - {2}", nome, T, P);
+            }
+
+
         }
     }
 }
