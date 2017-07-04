cf0730202605508f641108ce3b00f9bf4e5173ef
Arthur <cogupl4ys@gmail.com>
2017-07-04 16:01:05 -0300

    Começando Livros da Hermione e organizando a pasta 2C

---
+++ 2C/Livros de Hermione/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            int NroLivros = int.Parse(Console.ReadLine());
+            int pph = int.Parse(Console.ReadLine());
+            int hpd = int.Parse(Console.ReadLine());
+            Livro[] livros = new Livro [n];
+            for(i = 0; i <= NroLivros; i++)
+            {
+                Livro L = new Livro();
+                L.título = Console.ReadLine();
+                L.qtdPag = int.Parse(Console.ReadLine());
+                L.DiasDevol = int.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
