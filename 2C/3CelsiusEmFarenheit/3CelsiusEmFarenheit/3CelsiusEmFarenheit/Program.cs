dcbc56e62c60c19b54ede7e178423ea6ee6b5054
Arthur <cogupl4ys@gmail.com>
2017-08-01 15:57:51 -0300

    exercicio de conversão(incompleto)

---
+++ 2C/3CelsiusEmFarenheit/3CelsiusEmFarenheit/3CelsiusEmFarenheit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3CelsiusEmFarenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TempC t1 = new TempC();
+            Console.WriteLine("Informe a temperatura");
+            t1.valor = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("1 para nova temperatura \n2 para Fahrenheit \n3 para Kelvin \n0 sair");
+            int comando = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(t1.EmFarenheit());
+            Console.WriteLine(t1.EmKelvin());
+        }
+    }
+}
---
+++ 2C/3CelsiusEmFarenheit/3CelsiusEmFarenheit/3CelsiusEmFarenheit/TempC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3CelsiusEmFarenheit
+{
+    class TempC
+    {
+        public double valor;
+        public double EmFarenheit()
+        {
+            double f = 1.8 * valor + 32;
+            return f;
+        }
+        public double EmKelvin()
+        {
+            return valor + 273;
+        }
+    }
+}
