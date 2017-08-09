f33aab0505321570deed80516269ec5af06ca450
Arthur <cogupl4ys@gmail.com>
2017-08-08 22:44:41 -0300

    Celsius em Fahrenheit Completo

--- 2C/3CelsiusEmFarenheit/3CelsiusEmFarenheit/3CelsiusEmFarenheit/Program.cs
+++ 2C/3CelsiusEmFarenheit/3CelsiusEmFarenheit/3CelsiusEmFarenheit/Program.cs
@@ -10,15 +10,26 @@ namespace _3CelsiusEmFarenheit
     {
         static void Main(string[] args)
         {
+            int x = 0;
             TempC t1 = new TempC();
-            Console.WriteLine("Informe a temperatura");
-            t1.valor = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("1 para nova temperatura \n2 para Fahrenheit \n3 para Kelvin \n0 sair");
-            int comando = int.Parse(Console.ReadLine());
-
-
-            Console.WriteLine(t1.EmFarenheit());
-            Console.WriteLine(t1.EmKelvin());
+            while (true)
+            {
+                Console.WriteLine("1- Nova Temperatura");
+                Console.WriteLine("2- Para Fahrenheit");
+                Console.WriteLine("3- Para Kelvin");
+                Console.WriteLine("0- Sair");
+                x = int.Parse(Console.ReadLine());
+                if (x == 1)
+                    t1.valor = double.Parse(Console.ReadLine());
+                else if (x == 2)
+                    Console.WriteLine(t1.EmFarenheit());
+                else if (x == 3)
+                    Console.WriteLine(t1.EmKelvin());
+                else if (x == 0)
+                    break;
+                else
+                    Console.WriteLine("erro"); 
+            }
         }
     }
 }
