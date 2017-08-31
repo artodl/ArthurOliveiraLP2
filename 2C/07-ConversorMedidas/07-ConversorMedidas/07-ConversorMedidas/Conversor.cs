7cb23085f9a908cdbd07d5d5d36d5a12120620d1
Arthur <cogupl4ys@gmail.com>
2017-08-31 14:22:06 -0300

    Programa de Conversor(tem alguns commits no nome de outra pessoa, agora ta tudo certo, ajeitei as credenciais)

---
+++ 2C/07-ConversorMedidas/07-ConversorMedidas/07-ConversorMedidas/Conversor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ConversorMedidas
+{
+    class Conversor
+    {
+        public static double KparaF(double KF)
+        {
+            double FK = 1.8 * (KF - 273) + 32;
+            return FK;
+        }
+
+        public static double FparaK(double FK)
+        {
+            double KF = (FK + 459.67) * (5.0/9);
+            return KF;
+        }
+
+        public static double KparaC(double KC)
+        {
+            double CK = KC - 273;
+            return CK;
+        }
+
+        public static double CparaK(double CK)
+        {
+            double KC = CK + 273;
+            return KC;
+        }
+
+        public static double FparaC(double FC)
+        {
+            double CF = 1.8 * FC + 32;
+            return CF;
+        }
+
+        public static double CparaF(double CF)
+        {
+            double FC = 1.8 * CF + 32;
+            return FC;
+        }
+    }
+}
---
+++ 2C/07-ConversorMedidas/07-ConversorMedidas/07-ConversorMedidas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ConversorMedidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            //Console.WriteLine("1-Inserir Algo \n2 Converter Peso \n3-Converter Temperatura \n4-Converter Distancia");
+            //x = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
