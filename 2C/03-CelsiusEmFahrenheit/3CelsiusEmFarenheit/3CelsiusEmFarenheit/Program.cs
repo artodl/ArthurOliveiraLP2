025dbeb2d2e6edde25d13b97fe226644ead9b3e8
GabrielyO <gaby.o.nascimento@gmail.com>
2017-08-31 13:20:42 -0300

    corrigi o nome fahrenheit

---
+++ 2C/03-CelsiusEmFahrenheit/3CelsiusEmFarenheit/3CelsiusEmFarenheit/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3CelsiusEmFarenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            TempC t1 = new TempC();
+            while (true)
+            {
+                Console.WriteLine("1- Nova Temperatura");
+                Console.WriteLine("2- Para Fahrenheit");
+                Console.WriteLine("3- Para Kelvin");
+                Console.WriteLine("0- Sair");
+                x = int.Parse(Console.ReadLine());
+                if (x == 1)
+                    t1.valor = double.Parse(Console.ReadLine());
+                else if (x == 2)
+                    Console.WriteLine(t1.EmFarenheit());
+                else if (x == 3)
+                    Console.WriteLine(t1.EmKelvin());
+                else if (x == 0)
+                    break;
+                else
+                    Console.WriteLine("erro"); 
+            }
+        }
+    }
+}
