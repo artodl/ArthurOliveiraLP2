f4bafb80c91d345f41e873f283f0887242e33502
Arthur <cogupl4ys@gmail.com>
2017-09-05 15:55:53 -0300

    início exercicio 8

---
+++ 2C/08-Empresa/08-Empresa/08-Empresa/Empregado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Empresa
+{
+    class Empregado
+    {
+        public string Nome { get; set; }
+
+        public int CPF { get; set; }
+
+        public string Email { get; set; }
+
+        public string Salario { get; set; }
+
+        public string NroRegist { get; set; }
+
+        public string Setor { get; set; }
+
+        public string DtaEntrada { get; set; }
+    }
+}
