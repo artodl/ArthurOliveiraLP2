d0cec2be8ae81b130acb7802563188db4e28e13e
Arthur <cogupl4ys@gmail.com>
2017-11-07 15:56:59 -0300

    stuff

---
+++ Pessoal/idk/idk/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 
+
+namespace idk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection conexao = new MySqlConnection("Server=localhost;Database=bd;Uid=root;Pwd=");
+            MySqlCommand cmd = new MySqlCommand();
+
+            cmd.Connection = conexao;
+            cmd.CommandText = "Insert into (id,stuff)(3,damn)";
+
+            conexao.Open();
+            cmd.ExecuteNonQuery();
+            conexao.Close();
+
+            Console.WriteLine("DEU BOM");
+
+
+
+
+        }
+    }
+}
