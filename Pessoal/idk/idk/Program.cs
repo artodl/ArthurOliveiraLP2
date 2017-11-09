910018135ed8dc3d4e6fbf1cc9cf9f1d8b583e5a
Rafaela Gonçalves <rafaelaga46@gmail.com>
2017-11-09 15:27:28 -0200

    codigo funcionando yeah

--- Pessoal/idk/idk/Program.cs
+++ Pessoal/idk/idk/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using 
+using System.Data.SqlClient;
 
 namespace idk
 {
@@ -11,17 +11,17 @@ namespace idk
     {
         static void Main(string[] args)
         {
-            MySqlConnection conexao = new MySqlConnection("Server=localhost;Database=bd;Uid=root;Pwd=");
-            MySqlCommand cmd = new MySqlCommand();
+            SqlConnection conexao = new SqlConnection("Data Source=EN2LIA_15; Initial Catalog=idk; Integrated Security=SSPI");
+            SqlCommand cmd = new SqlCommand();
 
             cmd.Connection = conexao;
-            cmd.CommandText = "Insert into (id,stuff)(3,damn)";
+            cmd.CommandText = "INSERT INTO tabelaidk(id,stuff,shit) VALUES(1,'damn','son');";
 
             conexao.Open();
             cmd.ExecuteNonQuery();
             conexao.Close();
 
-            Console.WriteLine("DEU BOM");
+            Console.WriteLine("fon");
 
 
 
